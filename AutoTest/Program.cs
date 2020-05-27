790fc2505e2f2a553d2775983280129733ce7a4f
unknown <aleksandr.russkikh@gmail.com>
2020-05-28 00:48:01 +0300

    first commit

---
+++ AutoTest/Program.cs
@@ -0,0 +1,45 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Threading;
+
+namespace AutoTest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Создаём и запускаем Веб-Драйвер по адресу URL.
+            IWebDriver webDriver = new FirefoxDriver();
+            webDriver.Url = "http://the-internet.herokuapp.com/add_remove_elements/";
+
+            // Задержка для полной загрузки страницы.
+            Thread.Sleep(1000);
+
+            // 2 разa нажимаем кнопку "Add Element".
+            for (int i = 0; i < 2; ++i)
+            {
+                Thread.Sleep(1000); // Задержка перед повторным нажатием.
+                webDriver.FindElement(By.XPath("/html/body/div[2]/div/div/button")).Click();
+            }
+
+            // Подсчёт количества элементов в Классе.
+            var count = webDriver.FindElements(By.ClassName("added-manually")).Count;
+
+            // Удаляем ВСЕ добавленные кнопки Delete.
+            for (int i = 0; i < count; ++i)
+            {
+                Thread.Sleep(1000);
+                webDriver.FindElement(By.XPath("/html/body/div[2]/div/div/div/button")).Click();
+            }
+
+            // Повторно проверяем количество элементов в Классе.
+            count = webDriver.FindElements(By.ClassName("added-manually")).Count;
+
+            Thread.Sleep(1000);
+
+            // Закрываем Веб-Драйвер.
+            webDriver.Close();
+        }
+    }
+}
---
+++ AutoTest/Test.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System.Threading;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Support.UI;
+
+namespace AutoTest
+{
+    class Test
+    {
+        IWebDriver webDriver;
+
+        [SetUp]
+        public void StartBrowser()
+        {
+            webDriver = new FirefoxDriver();
+        }
+
+        [Test]
+        public void Testing()
+        {
+            webDriver.Url = "http://the-internet.herokuapp.com/add_remove_elements/";
+        }
+
+        [Test]
+        public void IsAdded()
+        {
+            webDriver.Url = "http://the-internet.herokuapp.com/add_remove_elements/";
+
+            for (int i = 0; i < 2; ++i)
+            {
+                Thread.Sleep(1000); // Задержка перед повторным нажатием.
+                webDriver.FindElement(By.XPath("/html/body/div[2]/div/div/button")).Click();
+            }
+
+            var count = webDriver.FindElements(By.ClassName("added-manually")).Count;
+            Assert.IsTrue(count == 2);
+        }
+
+        [Test]
+        public void IsDeleted()
+        {
+            webDriver.Url = "http://the-internet.herokuapp.com/add_remove_elements/";
+            // Добавляем 2 кнопки "Delete"
+            for (int i = 0; i < 2; ++i)
+            {
+                Thread.Sleep(1000); // Задержка перед повторным нажатием.
+                webDriver.FindElement(By.XPath("/html/body/div[2]/div/div/button")).Click();
+            }
+            // Удаляем 2 добавленные кнопки Delete.
+            for (int i = 0; i < 2; ++i)
+            {
+                Thread.Sleep(1000);
+                webDriver.FindElement(By.XPath("/html/body/div[2]/div/div/div/button")).Click();
+            }
+
+            var count = webDriver.FindElements(By.ClassName("added-manually")).Count;
+            Assert.IsTrue(count == 0);
+        }
+
+        [TearDown]
+        public void CloseBrowser()
+        {
+            webDriver.Close();
+        }
+    }
+}
+
